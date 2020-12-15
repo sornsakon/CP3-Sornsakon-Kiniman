975f4d96b249ae3ceb1d1de9ad28f37fb4e12b78
sornsakon <75887967+sornsakon@users.noreply.github.com>
2020-12-15 22:11:51 +0700

    Create Exercise4_Sornsakon_K.py

---
+++ Exercise4_Sornsakon_K.py
@@ -0,0 +1,10 @@
+FE_score = 79.75
+GB_score = 72.25
+CS_score = 68.80
+CP_score = 82.40
+2x = "dd"
+print("--- Your Score ---")
+print("Foundation English :",FE_score)
+print("General Business :",GB_score)
+print("Introduction to Computer Systems :",CS_score)
+print("Computer Programming :",CP_score)
