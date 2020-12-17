bb65d4d3c9aea04d995ef43143c5cb6d0b7224b7
sornsakon <75887967+sornsakon@users.noreply.github.com>
2020-12-17 23:49:07 +0700

    Create Exercise5_1_Sornsakon_K.py

---
+++ Exercise5_1_Sornsakon_K.py
@@ -0,0 +1,12 @@
+a = 10
+b = 5
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+print("1st Number",a)
+print("2nd Number",b)
+print(str(a),"+",str(b),"=",c)
+print(str(a),"-",str(b),"=",d)
+print(str(a),"*",str(b),"=",e)
+print(str(a),"/",str(b),"=",int(f))
