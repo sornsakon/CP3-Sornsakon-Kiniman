cd68b025896e16f6346418b58ce0392ea636d71e
sornsakon <75887967+sornsakon@users.noreply.github.com>
2020-12-17 23:50:23 +0700

    Update Exercise5_1_Sornsakon_K.py

--- Exercise5_1_Sornsakon_K.py
+++ Exercise5_1_Sornsakon_K.py
@@ -4,9 +4,9 @@ c = a+b
 d = a-b
 e = a*b
 f = a/b
-print("1st Number",a)
-print("2nd Number",b)
+print("1st Number =",a)
+print("2nd Number =",b)
 print(str(a),"+",str(b),"=",c)
 print(str(a),"-",str(b),"=",d)
 print(str(a),"*",str(b),"=",e)
-print(str(a),"/",str(b),"=",int(f))
+print(str(a),"/",str(b),"=",f)
