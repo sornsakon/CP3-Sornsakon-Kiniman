4b67a64d3c3955aeb937af7e520d3b9030e45f32
sornsakon <75887967+sornsakon@users.noreply.github.com>
2020-12-21 23:07:06 +0700

    Create Exercise_Lec50_Sornsakon_K.py

---
+++ Exercise_Lec50_Sornsakon_K.py
@@ -0,0 +1,16 @@
+def addnum(x,y):
+    print(x + y)
+def minusnum(x,y):
+    print(x - y)
+def multiplenum(x,y):
+    print(x * y)
+def dividenum(x,y):
+    print(x / y)
+print("--- Add ---")
+addnum(int(input("x = ")), int(input("y = ")))
+print("--- Minus ---")
+minusnum(int(input("x = ")), int(input("y = ")))
+print("--- Multiple ---")
+multiplenum(int(input("x = ")), int(input("y = ")))
+print("--- Divide ---")
+dividenum(int(input("x = ")), int(input("y = ")))
