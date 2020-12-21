b9cfbc16489ee66d0722193eb7d0cfd7f53e21a7
sornsakon <75887967+sornsakon@users.noreply.github.com>
2020-12-21 23:46:13 +0700

    Create Exercise_Lec54_Sornsakon_K.py

---
+++ Exercise_Lec54_Sornsakon_K.py
@@ -0,0 +1,36 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return True
+    else:
+        return False
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    return userSelected
+
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+
+while login():
+    showMenu()
+    select = menuSelect()
+    if select == 1:
+        print(vatCalculator(int(input("Total Price : "))))
+        break
+    elif select == 2:
+        print(priceCalculator())
+        break
