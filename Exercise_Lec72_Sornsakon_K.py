00d6a3743e3bc77d1b467067609254d76514ac91
sornsakon <75887967+sornsakon@users.noreply.github.com>
2020-12-23 19:32:21 +0700

    Create Exercise_Lec72_Sornsakon_K.py

---
+++ Exercise_Lec72_Sornsakon_K.py
@@ -0,0 +1,18 @@
+menuList = []
+def showBill():
+    total = 0
+    print("---- My Food----")
+    for number in range(len(menuList)):
+        print(menuList[number][0], menuList[number][1])
+        total += int(menuList[number][1])
+    print("Total Cost :",total)
+
+while True:
+    menuName = input("Plese Enter Menu :")
+    if(menuName.lower() == "exit"):
+        break
+    else:
+        menuPrice = input("Price :")
+        menuList.append([menuName,menuPrice])
+
+showBill()
